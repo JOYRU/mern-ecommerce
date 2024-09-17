8ec97dd36263fd20d2b373ea7370c220636442cc
JOYRU <joycseru@gmail.com>
2024-09-17 11:31:58 +0600

    work start with front-end

---
+++ client/src/routes/index.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+
+const Index = () => {
+    return(
+        <BrowserRouter>
+           <Routes>
+                <Route />
+                <Route />
+                <Route />
+
+            </Routes>
+        </BrowserRouter>
+
+    );
+};
+
+export default Index
